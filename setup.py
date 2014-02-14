f9e47d3b73ca8bb4d154a7886f2c12929fb98278
Tom <tom@vistarmedia.com>
2014-02-14 15:42:51 -0500

    Trying to fix testing issue

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ setup(
   packages  = ['rest'],
 
   install_requires = [
-    'Flask         == 0.10',
+    'Flask         == 0.9',
   ],
 
   tests_require = [
+    '-e git+https://github.com/jarus/flask-testing.git@7bf34b039cb93b5447073c12243f9f4debc14a6c#egg=Flask-Testing',
     'Flask-Testing == 0.4',
     'nose          == 1.3.0',
   ],
